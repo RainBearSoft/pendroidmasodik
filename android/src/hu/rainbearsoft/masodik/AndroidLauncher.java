3051cac9b9da541f29475145218d29e927e21de8
istvan12345678 <tothistvan12345678@gmail.com>
2016-11-11 15:15:04 +0100

    Projekt létrehozva

---
+++ android/src/hu/rainbearsoft/masodik/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package hu.rainbearsoft.masodik;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import hu.rainbearsoft.masodik.Main;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Main(), config);
+	}
+}
---
+++ core/src/hu/rainbearsoft/masodik/Main.java
@@ -0,0 +1,12 @@
+package hu.rainbearsoft.masodik;
+import com.badlogic.gdx.Game;
+
+public class Main extends Game {
+        @Override
+        public void create() {
+            System.out.println("szia");
+        }
+        @Override
+        public void dispose() {
+        }
+}
