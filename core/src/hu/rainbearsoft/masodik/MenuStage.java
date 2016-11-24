e49e994bb23bf7044e3353e2a0b4afbcfbc5b8ef
Martin <pigfighter2@gmail.com>
2016-11-24 15:12:24 +0100

    screen

---
+++ core/src/hu/rainbearsoft/masodik/MenuStage.java
@@ -0,0 +1,48 @@
+package hu.rainbearsoft.masodik;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.mygdx.game.DemoGame.GameScreen;
+import com.mygdx.game.DemoInput.InputScreen;
+import com.mygdx.game.MyBaseClasses.MyButton;
+import com.mygdx.game.MyBaseClasses.MyLabel;
+import com.mygdx.game.MyBaseClasses.MyScreen;
+import com.mygdx.game.MyBaseClasses.MyStage;
+import com.mygdx.game.MyBaseClasses.OneSpriteAnimatedActor;
+import com.mygdx.game.DemoOtherScr.OtherScreen;
+import com.mygdx.game.MyBaseClasses.ShapeType;
+import com.mygdx.game.MyGdxGame;
+
+/**
+ * Created by tuskeb on 2016. 09. 30..
+ */
+public class MenuStage extends MyStage {
+
+
+    public MenuStage(Viewport viewport, Batch batch, MyGdxGame game) {
+        super(viewport, batch, game);
+    }
+
+
+    public void init()
+    {
+
+    }
+
+
+    @Override
+    public void act(float delta) {
+        super.act(delta);
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+
+    }
+}
